7b2bc42789ced5d7248621bc3a09d2f5267bc864
David Cattermole <davidcattermole@gmail.com>
2012-07-02 14:40:20 +0100

    Single login page

--- js/script.js
+++ js/script.js
@@ -6,6 +6,23 @@
     // carousel demo
     $('#quoteCarousel').carousel()
 
+    // Javascript to enable link to tab
+    var hash = document.location.hash;
+    var prefix = "";
+    if (hash) {
+        $('.nav-tabs a[href='+hash.replace(prefix,"")+']').tab('show');
+    } 
+
+    // Change hash for page-reload
+    $('.nav-tabs a').on('shown', function (e) {
+        window.location.hash = e.target.hash.replace("#", "#" + prefix);
+    });
+
+    $('.forgotLink').click(function (e) {
+      e.preventDefault();
+      $('.nav-tabs a').tab('show');
+    });
+
 
   });
 
