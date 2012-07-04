d9e36693adb716fc79f4cf221461e306d3b89c1f
David Cattermole <davidcattermole@gmail.com>
2012-07-05 00:16:14 +0100

    Add http:// for good measure

--- includes/nav.php
+++ includes/nav.php
@@ -7,13 +7,9 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </a>
-
-          <!-- Control Panel button always visible -->
           
-          <a class="pull-right btn btn-small btn-inverse" href="<?php echo "cpanel." . $siteUrl ?>"><i class="icon-cog icon-white"></i> Control <span class="hidden-phone">Panel</span></a>
+          <a class="pull-right btn btn-small btn-inverse" href="<?php echo "http://cpanel." . $siteUrl ?>"><i class="icon-cog icon-white"></i> Control <span class="hidden-phone">Panel</span></a>
 
-          <!-- Your website name. If you have a logo you could change this to an img. 
-          Change the link to point to the root of your domain -->
           <a class="brand" href="<?php echo $siteUrl ?>"><?php echo $siteName ?></a>
 
           <div class="nav-collapse">
@@ -27,7 +23,7 @@
               <li <?php if ($thisPage == "Contact") echo "class=\"active\""; ?>><a href="contact.php">Contact</a></li>
             </ul>
 
-            <a class="pull-right btn btn-small btn-info" href="<?php echo "webmail." . $siteUrl ?>"><i class="icon-envelope icon-white"></i> Webmail</a>
+            <a class="pull-right btn btn-small btn-info" href="<?php echo "http://webmail." . $siteUrl ?>"><i class="icon-envelope icon-white"></i> Webmail</a>
 
           </div>
 
